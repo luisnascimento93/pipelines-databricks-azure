fd3028858878c550c6af11baef5a5a85bd6f6c79
luisnascimento93@gmail.com <luisnascimento93@gmail.com>
2024-02-12 18:31:04 +0000

    feat: pipeline notebooks

---
+++ notebooks/inbound_to_bronze.scala
@@ -0,0 +1,67 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ##### Conferindo se dos dados foram montados e se temos acesso a pasta inbound
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC
+// MAGIC dbutils.fs.ls("/mnt/dados/inbound")
+// MAGIC
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC #### Lendo os dados na camada de inbound
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/inbound/dados_brutos_imoveis.json"
+val dados = spark.read.json(path)
+
+// COMMAND ----------
+
+display(dados)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC #### Removendo colunas
+// MAGIC
+
+// COMMAND ----------
+
+
+val dados_anuncio = dados.drop("imagens", "usuario")
+
+display(dados_anuncio)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC #### Criando uma coluna de identificação
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions.col
+
+// COMMAND ----------
+
+
+val df_bronze = dados_anuncio.withColumn("id", col("anuncio.id"))
+display(df_bronze)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC #### Salvando na camada bronze
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/bronze/dataset_imoveis"
+
+df_bronze.write.format("delta").mode(SaveMode.Overwrite).save(path)
+
+// COMMAND ----------
+
+
